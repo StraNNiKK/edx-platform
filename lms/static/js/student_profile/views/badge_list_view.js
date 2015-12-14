09b476c12c934481624c16b1345534a66eb5315a
Sven Marnach <sven@marnach.net>
2015-12-14 18:40:52 +0100

    Fix the badge list view for even numbers of badges.

--- lms/static/js/student_profile/views/badge_list_view.js
+++ lms/static/js/student_profile/views/badge_list_view.js
@@ -7,38 +7,29 @@
             var BadgeListView = ListView.extend({
                 'tagName': 'div',
                 renderCollection: function () {
-                    this.$el.html('');
-                    var row = $('<div class="row">');
-                    var append_placeholder = ! this.collection.hasNextPage();
-                    var make_last_row = true;
+                    this.$el.empty();
+                    var row;
                     // Split into two columns.
                     this.collection.each(function (badge, index) {
-                        /*jshint -W018 */
-                        var make_row = (index && !(index % 2));
-                        if (make_row) {
-                            this.$el.append(row);
+                        if (index % 2 === 0) {
                             row = $('<div class="row">');
-                            make_last_row = false;
-                        } else {
-                            make_last_row = true;
+                            this.$el.append(row);
                         }
                         var item = new BadgeView({model: badge}).render().el;
                         row.append(item);
                         this.itemViews.push(item)
                     }, this);
                     // Placeholder must always be at the end, and may need a new row.
-                    var placeholder = _.template(
-                        badgePlaceholder,  {find_courses_url: this.options.find_courses_url}
-                    );
-                    if (! append_placeholder) {
-                        return;
+                    if (!this.collection.hasNextPage()) {
+                        var placeholder = _.template(
+                            badgePlaceholder,  {find_courses_url: this.options.find_courses_url}
+                        );
+                        if (this.collection.length % 2 === 0) {
+                            row = $('<div class="row">');
+                            this.$el.append(row);
+                        }
+                        row.append(placeholder);
                     }
-                    if (make_last_row) {
-                        this.$el.append(row);
-                        row = $('<div class="row">');
-                    }
-                    row.append(placeholder);
-                    this.$el.append(row);
                     return this;
                 }
             });
