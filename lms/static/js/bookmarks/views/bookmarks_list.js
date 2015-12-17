845c2177abc8b2de89270949457688274e7eaa10
Ehtesham <ehtesham.kafeel@gmail.com>
2015-12-17 18:15:39 +0500

    Removing loading message

--- lms/static/js/bookmarks/views/bookmarks_list.js
+++ lms/static/js/bookmarks/views/bookmarks_list.js
@@ -55,14 +55,11 @@
 
                 this.hideErrorMessage();
                 this.showBookmarksContainer();
-                this.showLoadingMessage();
 
                 this.collection.goTo(this.defaultPage).done(function () {
-                    view.hideLoadingMessage();
                     view.render();
                     view.focusBookmarksElement();
                 }).fail(function () {
-                    view.hideLoadingMessage();
                     view.showErrorMessage();
                 });
             },
@@ -100,7 +97,7 @@
             hideBookmarks: function () {
                 this.$el.hide();
                 $(this.coursewareResultsWrapperEl).hide();
-                $(this.coursewareContentEl).css( 'display', 'table-cell')
+                $(this.coursewareContentEl).css( 'display', 'table-cell');
             },
 
             showBookmarksContainer: function () {
@@ -108,7 +105,7 @@
                 // Empty el if it's not empty to get the clean state.
                 this.$el.html('');
                 this.$el.show();
-                $(this.coursewareResultsWrapperEl).css('display', 'table-cell')
+                $(this.coursewareResultsWrapperEl).css('display', 'table-cell');
             },
 
             showLoadingMessage: function () {
